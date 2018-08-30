6411d01856ca9ab840b772dda146ec08d9c81b36
DESKTOP-ECD68R3\COMLABPC15 <Jakerenzocarlos01@gmail.com>
2018-08-30 19:34:16 +0800

    Complete Projects For Prelim/ Jakerenzo.Shutdow.Web

---
+++ JakeRenzo.Shutdown.Web/ViewModels/Users/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using JakeRenzo.Shutdown.Web.Infrastructure.Data.Helpers;
+using JakeRenzo.Shutdown.Web.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JakeRenzo.Shutdown.Web.ViewModels.Users
+{
+    public class IndexViewModel
+    {
+        public Page<User> Users { get; set; }
+    }
+}
